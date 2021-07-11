eec157acaa065298d22a2ec1dffbd353720559b9
probopastor <43503592+probopastor@users.noreply.github.com>
2021-07-10 23:16:33 -0500

    Set up window

---
+++ SpookyVR/Assets/Scripts/Background Scripts/Billboard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    [SerializeField, Tooltip("If true, billboard will maintain the same rotation as the main camera. ")] private bool useStaticBillboard = true;
+    [Tooltip("The main camera. ")] private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if(!useStaticBillboard)
+        {
+            transform.LookAt(mainCamera.transform);
+        }
+        else
+        {
+            transform.rotation = mainCamera.transform.rotation;
+        }
+
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+    }
+}
