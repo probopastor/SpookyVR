13d71e536e7bccc6ddb00a53977e4708b0454f94
probopastor <43503592+probopastor@users.noreply.github.com>
2021-09-08 16:41:43 -0500

    Resource Bank & Location Superclass Created
    - Created a Resource Bank that handles all resources produced by locations.
    
    - Created a superclass called OtloLocations to handle individual location methods / resources. (For example, each loation will have its own value of Prosperity, its own values for the resources they produce, etc.)
    
    - Set up Getters and Setters for OtloLocation resources.

---
+++ SpookyVR/Assets/Scripts/Resource Scripts/ResourceBank.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceBank : MonoBehaviour
+{
+    #region Otlo Resources
+    [Tooltip(" ")] private int population = 0;
+    [Tooltip(" ")] private int money = 0;
+    [Tooltip(" ")] private int food = 0;
+    [Tooltip(" ")] private int militia = 0;
+    [Tooltip(" ")] private int crimeRate = 0;
+    [Tooltip(" ")] private int cultPresence = 0;
+    #endregion 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
