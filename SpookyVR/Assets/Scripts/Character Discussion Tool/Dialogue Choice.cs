ada92415a9540984fd789ce2eb41431cb3d14234
CJ Green <55067215+Godofwar1300@users.noreply.github.com>
2021-09-18 13:24:22 -0500

    Commit #3
    - Started Coding Character Dialogue Tool.
    - Made a Level Manager Prefab.

---
+++ SpookyVR/Assets/Scripts/Character Discussion Tool/Dialogue Choice.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  class DialogueChoice : MonoBehaviour
+{
+
+    private Character currentCharacter = null;
+
+
+    private void Awake()
+    {
+        if(currentCharacter == null)
+        {
+            
+        }
+    }
+    #region Start and Update
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+
+
+
+}
