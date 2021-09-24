f6d8c58d9ee4c5f0c23462c9eae875f32788dbee
probopastor <43503592+probopastor@users.noreply.github.com>
2021-09-23 21:57:15 -0500

    Simplified Character Text SO

--- SpookyVR/Assets/Scripts/Character Discussion Tool/CharacterDialogue.cs
+++ SpookyVR/Assets/Scripts/Character Discussion Tool/CharacterDialogue.cs
@@ -1,6 +1,6 @@
 ﻿/* 
 * Glory to the High Council
-* CJ Green
+* CJ Green, William Nomikos
 * CharacterDialogue.cs
 * ...Description of the script...
 * ...
@@ -14,87 +14,22 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "Character Dialogue", menuName = "Character Dialogue", order = 10)]
 public class CharacterDialogue : ScriptableObject
 {
-    public List<Image> characterDialogueImages = new List<Image>();
-    public List<Interactions> characterDialogueInteractions = new List<Interactions>(1);
-}
+    [SerializeField]
+    private List<Interactions> introDialog = new List<Interactions>(1);
 
-[System.Serializable]
-public class TextState
-{
-    public string text;
-
-    public Image characterState;
-
-
+    [SerializeField]
+    private List<Interactions> mainDialog = new List<Interactions>(1);
 }
 
 [System.Serializable]
 public class Interactions
 {
+    [SerializeField] private List<TextState> introDialogue = new List<TextState>(1);
+}
 
-    #region Character Interaction Dialogue Containers
-    [System.Serializable]
-    private class IntroInteractions
-    {
-        [SerializeField]
-        private int index = 0;
-
-        [SerializeField,TextArea(minLines: 5, maxLines: 20)]
-        public List<TextState> introDialogue = new List<TextState>(1);
-
-
-
-        public int GetCurrentListIndex()
-        {
-            return index;
-        }
-
-        //public void SetIndex()
-        //{
-
-        //}
-
-    }
-
-    [System.Serializable]
-    private class MainDialogue
-    {
-        [SerializeField]
-        private int index = 0;
-
-        [SerializeField, TextArea(minLines: 5, maxLines: 20)]
-        public List<string> mainDialogue = new List<string>(1);
-
-        public int GetCurrentListIndex()
-        {
-            return index;
-        }
-
-    }
-
-    [System.Serializable]
-    private class Responses
-    {
-        [SerializeField]
-        private int index = 0;
-
-        [TextArea(minLines: 1, maxLines: 5)]
-        public List<string> responses = new List<string>(1);
-
-        public int GetCurrentListIndex()
-        {
-            return index;
-        }
-
-    }
-    #endregion
-
-    [SerializeField]
-    private List<IntroInteractions> intro_Interactions = new List<IntroInteractions>(1);
-    
-    [SerializeField]
-    private List<MainDialogue> mainDialogue_Interactions = new List<MainDialogue>(1);
-    
-    [SerializeField]
-    private List<Responses> responses_interactions = new List<Responses>(1);
+[System.Serializable]
+public class TextState
+{
+    [SerializeField, TextArea(minLines: 5, maxLines: 20)] private string text;
+    [SerializeField] private Image characterState;
 }
