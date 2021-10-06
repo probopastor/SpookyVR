baad7d4479038b35045f263976dbd59b7b6a20d9
probopastor <43503592+probopastor@users.noreply.github.com>
2021-10-06 06:29:31 -0500

    Began to restructure dialog

---
+++ SpookyVR/Assets/Scripts/Character Discussion Tool/DialogTextSetter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTextSetter : MonoBehaviour
+{
+    [SerializeField, Tooltip(" ")] private List<Interactions> interactionsToQueue = new List<Interactions>();
+    //[SerializeField, Tooltip(" ")] private List<TextState> dialogTextStatesToQueue = new List<TextState>();
+}
