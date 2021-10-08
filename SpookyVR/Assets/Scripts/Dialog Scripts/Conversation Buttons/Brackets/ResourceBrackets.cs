718ae75b210b541080f7ab679147ad3237648366
probopastor <43503592+probopastor@users.noreply.github.com>
2021-10-07 23:47:07 -0500

    Began setting up text setter

---
+++ SpookyVR/Assets/Scripts/Dialog Scripts/Conversation Buttons/Brackets/ResourceBrackets.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceBrackets : MonoBehaviour
+{
+    [SerializeField, Tooltip("The minimum bound of this bracket. ")] private int bracketMin = 0;
+    [SerializeField, Tooltip("The maximum bound of this bracket. ")] private int bracketMax = 0;
+
+    [SerializeField, Tooltip("The index of interactions to be called when the resource falls into this bracket (between min and max bounds). ")] private int interactionIndex = 0;
+}
---
+++ SpookyVR/Assets/Scripts/Dialog Scripts/Conversation Buttons/DialogTextSetter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DialogTextSetter : MonoBehaviour
+{
+    [SerializeField, Tooltip("All interactions this button can have. ")] private List<Interactions> interactions = new List<Interactions>();
+
+    [Space(20)]
+    [Header("Interaction Changers - Whether the following variables should change which Interaction the interactions should use.")]
+    [Space(20)]
+
+    [Space(10)]
+    [Header("Change Interaction - Money")]
+    [Space(10)]
+    [SerializeField, Tooltip("Whether money should be checked for when deciding which interaction to use. ")] private bool checkMoney = false;
+    [SerializeField, Tooltip("The money brackets to check. ")] private ResourceBrackets[] moneyBrackets;
+
+    [Space(10)]
+    [Header("Change Interaction - Militia")]
+    [Space(10)]
+    [SerializeField, Tooltip("Whether militia should be checked for when deciding which interaction to use. ")] private bool checkMilitia = false;
+    [SerializeField, Tooltip("The money brackets to check. ")] private ResourceBrackets[] militiaBrackets;
+
+    [Space(10)]
+    [Header("Change Interaction - Prosperity")]
+    [Space(10)]
+    [SerializeField, Tooltip("Whether prosperity should be checked for when deciding which interaction to use. ")] private bool checkProsperity = false;
+    [SerializeField, Tooltip("The money brackets to check. ")] private ResourceBrackets[] prosperityBrackets;
+
+    public List<Interactions> GetInteractions()
+    {
+        return interactions;
+    }
+}
