79ecf61618c3bbfcbf3263e0916f9398975c466f
probopastor <43503592+probopastor@users.noreply.github.com>
2021-10-30 18:54:32 -0500

    Created TextState parsing and ConversationTopicButtons

---
+++ SpookyVR/Assets/Scripts/Dialog Scripts/Conversation Buttons/ConversationTopicButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationTopicButton : MonoBehaviour
+{
+    // Order:
+    // 1. Conversation Button contains List<Interactions> to use if selected
+    // 2. Conversation Button appears only during timelines the array narrativeBranchesIncludedIn match with. This is checked via a manager. 
+    // 3. When selected, the correct interaction is chosen from the List<Interactions> via the TopicParser class. This parse will mainly check Trust. 
+    // 4. Dialog will be ran from the TopicParser. 
+    // 5. When this is started, all buttons vanish as text is in session. 
+
+
+    [SerializeField, Tooltip("An array of narrative branches this conversation option should appear in. ")] private int[] narrativeBranchesIncludedIn;
+    [SerializeField, Tooltip("A list of the possible interactions this conversation option will contain. ")] private List<Interactions> dialogConversations;
+
+    [Tooltip("TopicParser reference, which is used to determine which Interaction to use when this conversation option is selected. ")] private TopicParser topicParser;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        topicParser = FindObjectOfType<TopicParser>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectConversation()
+    {
+
+    }
+}
---
+++ SpookyVR/Assets/Scripts/Dialog Scripts/Conversation Buttons/TopicParser.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopicParser : MonoBehaviour
+{
+    [Tooltip(" ")] private DialogHandler dialogHandler;
+    [Tooltip(" ")] private List<TextState> parsedTextState = new List<TextState>(); 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogHandler = FindObjectOfType<DialogHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Determines which text state, from a list of text states, should be chosen based on certain variables.
+    /// </summary>
+    /// <param name=""></param>
+    /// <returns></returns>
+    public List<TextState> ParseInteractions(List<Interactions> interactions)
+    {
+
+        return parsedTextState;
+    }
+
+    public void StartDialog()
+    {
+        StartCoroutine(dialogHandler.RunDialog(parsedTextState));
+    }
+}
